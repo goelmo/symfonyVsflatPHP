0be89700fc7585dd94596c864e9fd111871e14a8
AH <amanuel@360ground.com>
2016-11-19 05:18:50 +0800

    add comment with betty

--- index.php
+++ index.php
@@ -7,14 +7,21 @@ require_once 'controllers.php';
 // route the request internally
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
+echo $uri;
+//show where the url
+
 //if ('/index.php' === $uri) {
 if ('/symfonyVsflatPHP/index.php' === $uri) {
     // Merhawi revison : add the folder name so that it could start loading form then
 	list_action();
     // '/pattern/'
 } elseif (preg_match('/\/symfonyVsflatPHP\/index.php\/show\/(\d+)/', $uri, $matches)) {
+    //match the regular expression  using the function
+    //@parameter with escape key (\) to find the ID number
     $id = $matches[1];
+    //to store result ID
 	show_action($id);
+    //to show the id on the page
 } else {
 	header('HTTP/1.1 404 Not Found');
 	echo '<html><body><h1>Page Not Found</h1></body></html>';
